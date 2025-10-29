5e943e3aa97a6a68e20fcea685e84ce4c8d46a2b
buzzz1995-ops <buzzz1995@gmail.com>
2025-10-29 11:01:46 +0000

    Update views.py

--- galeria/views.py
+++ galeria/views.py
@@ -1,13 +1,8 @@
 from flask import render_template, request, jsonify
-from app import app
+from app import app, mail
 from flask_mail import Message
 
 # Rota principal -> abre templates/index.html
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# Rota Ruben -> abre templates/ruben/index.html
-@app.route('/ruben')
-def ruben():
     return render_template('index.html')
