3e9a573d6eff02d2acdf66d011ca043dead2bc33
buzzz1995-ops <buzzz1995@gmail.com>
2025-10-29 11:02:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
-import os
+from flask_mail import Mail
 
 app = Flask(__name__)
+
+# ===== CONFIGURAÇÃO DO FLASK-MAIL =====
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'ruben-silva-work@wide-empire-474509-u6.iam.gserviceaccount.com'
+app.config['MAIL_PASSWORD'] = 'AIzaSyBo_uP-F10wgJ9qOTFhO2HpZIv1ByzXir4'                # senha de app do Gmail
+app.config['MAIL_DEFAULT_SENDER'] = ('Portfólio Ruben Silva', 'teuemail@gmail.com')
+
+mail = Mail(app)
 
 # importa as rotas depois de criar o app
 from galeria.views import *
